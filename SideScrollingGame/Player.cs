b2594f5ef58a3e96fcef124b2788640fe41085fd
KoraggX <KoraggX@users.noreply.github.com>
2019-07-10 08:53:49 +1000

    DmiDamn

--- SideScrollingGame/Player.cs
+++ SideScrollingGame/Player.cs
@@ -91,7 +91,7 @@ namespace SideScrollingGame
 
             if (player.Y + player.Height > game.screenHight)
             {
-                game.state = GameState.LOST;
+                // game.state = GameState.LOST;
             }
 
             if (player.Y < 0)
